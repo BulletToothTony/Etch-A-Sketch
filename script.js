13bd5245cfe4e661cdf129ab28782a931d96a64b
Henry WL <henrywl@gmail.com>
2021-01-07 02:39:44 +0000

    Added basic hover function

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Script
+
+const grid = document.querySelector('.grid');
+const resetbutton = document.querySelector("#reset-grid")
+const newsquar = document.querySelector(".square")
+
+window.addEventListener("load", createGrid())
+
+function createGrid() {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "iddivtest")
+        div.classList.add("square");
+        div.addEventListener("mouseover", changeColor)
+        grid.appendChild(div);
+    }
+};
+
+function changeColor(e) {
+    console.log(e)
+    e.target.className = "square-color"
+    // e.target.style.backgroundColor = "green"
+}
+
+// document.getElementById("square").addEventListener("mouseover", mouseoverfun)
+
+// // newsquar.addEventListener("mouseover", mouseoverfun);
+
+// function mouseoverfun(e) {
+//     e.target.style.backgroundColor = "red";
+// }
+
+
+
+//Add event listener to div, change colour
